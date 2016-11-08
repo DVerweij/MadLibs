73af15a1404a97a3911a783e9b659c9b361bd3c5
DVerweij <danyllo.verweij@student.uva.nl>
2016-11-08 13:11:34 +0100

    -Function that reads files, currently incomplete -Story reading to be implemented

--- app/src/main/java/com/example/danyllo/madlibs/LibsActivity.java
+++ app/src/main/java/com/example/danyllo/madlibs/LibsActivity.java
@@ -1,7 +1,13 @@
 package com.example.danyllo.madlibs;
 
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
+
 
 public class LibsActivity extends AppCompatActivity {
 
@@ -9,5 +15,15 @@ public class LibsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_libs);
+        Story storyObj = new Story(selectStory());
+    }
+    private java.io.InputStream selectStory(){
+        AssetManager ast = getApplicationContext().getAssets();
+        try {
+            return ast.open("madlib1_tarzan.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
